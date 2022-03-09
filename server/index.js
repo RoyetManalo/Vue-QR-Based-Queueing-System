eefbd58fe85d0774396174a26a184c1d19d6a10e
Royet Manalo <royetmanalo@gmail.com>
2022-03-09 17:49:48 +0800

    Version 1.0.0

--- server/index.js
+++ server/index.js
@@ -28,14 +28,14 @@ app.use("/api/vaccine", vaccineRoutes);
 const recentlyServedRoutes = require("./routes/api/recentlyServed");
 app.use("/api/recentlyserved", recentlyServedRoutes);
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/api/static", express.static(path.join(__dirname, "imageStatic")));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use("/api/static", express.static(path.join(__dirname, "imageStatic")));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("public"));
-//   app.use("/images", express.static("img"));
-//   app.get(/.*/);
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "public")));
+  app.use("/api/static", express.static(path.join(__dirname, "imageStatic")));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running in PORT:${PORT}`));
